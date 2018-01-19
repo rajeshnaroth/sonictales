8fdf705d5f80816838dd0079414e137aeb99c280
Rajesh Naroth <rajeshnaroth@gmail.com>
2018-01-19 00:35:43 -0800

    New files

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var path = require('path')
 
-app.get('/', (req, res) => res.send('Hello'));
-app.listen(3333, () => console.log('Listening on 3333'));
+var app = express()
+app.set('view engine', 'pug')
+app.set("views", path.join(__dirname, "views"))
+
+app.get('/', (req, res) => {
+    console.log(req.headers)
+    res.render('index', { title:'Sonic Tales', message: 'hello'})
+})
+
+app.listen(3333, () => console.log('Listening on 3333'))
